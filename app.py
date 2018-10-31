8028ad3497ee2bdafc17fb0f91b34ccef90fd61c
cjj272 <cjj272@gmail.com>
2018-10-31 16:25:44 -0400

    db, other cosmetic changes

--- app.py
+++ app.py
@@ -1,24 +1,49 @@
 import os
+import requests
+import sys
 from datetime import datetime
-from flask import Flask,render_template, redirect, url_for,session
+from flask import Flask,render_template, redirect, url_for,session,flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField,SubmitField,RadioField
+from wtforms.validators import DataRequired,Required,Length
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config.from_object(os.environ['APP_SETTINGS'])
+app.config.from_object('config.Config')
 
+db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
+
+#db stuff
+class SummonerToAccountID(db.Model):
+	__tablename__='sumtoacc'
+	name = db.Column(db.String(64),primary_key=True)
+	id = db.Column(db.Integer,index=True)
+	def __repr__(self):
+		return '<Name %r>' % self.name
+
+#riot stuff
+def GetSummonerInfo(accountName):
+	r=requests.get('https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + accountName,headers = {
+		"X-Riot-Token": "RGAPI-44250bf4-2e19-4f3d-a81b-46afbe60a985",
+	})
+
+	if r.status_code != 200:
+		return "Fail"
+	return (r.json().get('name'), r.json().get('accountId'))
+
+
 #form
 class CompForm(FlaskForm):
-	comp = StringField('Company name', validators=[DataRequired()])
-	submit = SubmitField('Check')
+	name = StringField('LoL name', validators=[DataRequired(),Length(2,15)])
+	#platform = RadioField('Platform', choices=[('7','7 day'),('15','15 day'),('30','30 day')],validators=[DataRequired()])
+	submit = SubmitField('Submit')
 
 
 #handle 404, 500
@@ -34,10 +59,30 @@ def internal_server_error(e):
 def index():
 	form=CompForm()
 	if form.validate_on_submit():
-		session['comp']=form.comp.data
-		comp=form.comp.data
+		session['name']=form.name.data
+		#session['platform']=form.platform.data
+
+		#check if user exists
+		exists = SummonerToAccountID.query.filter(SummonerToAccountID.name.ilike("%"+form.name.data+"%")).first() is not None
+		print(exists, file=sys.stdout)
+		if exists:
+			flash("User already exists",'danger')
+			return redirect(url_for('index'))
+
+
+		info = GetSummonerInfo(form.name.data)
+		if info=="Fail":
+			flash('Submit failed. User does not exist or Riot does not like us','danger')
+			return redirect(url_for('index'))
+		
+		user = SummonerToAccountID(name=info[0],id=info[1])
+		db.session.add(user)
+		db.session.commit()
+
+		flash('Success?','success')
+
 		return redirect(url_for('index'))
-	return render_template('index.html',current_time=datetime.utcnow(),form=form,comp=session.get('comp'))
+	return render_template('index.html',current_time=datetime.utcnow(),form=form,name=session.get('name'),platform=session.get('platform'))
 
 @app.route('/<name>')
 def user(name):
--- config.py
+++ config.py
@@ -7,8 +7,12 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'Fn9kmuYCbgodkUoYgsmWKgSg5t7zjQqLyX6f2sHAey4SC2ukfo4Rjw4aJFxGQhL6'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    RIOT_API_KEY = 'RGAPI-44250bf4-2e19-4f3d-a81b-46afbe60a985'
 
 
+'''
 class ProductionConfig(Config):
     DEBUG = False
 
@@ -25,3 +29,4 @@ class DevelopmentConfig(Config):
 
 class TestingConfig(Config):
     TESTING = True
+'''
