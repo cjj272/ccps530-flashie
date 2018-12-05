bd7a7a4ff6df4f0da0cfa41cc795728ac3eba916
cjj272 <cjj272@gmail.com>
2018-12-04 19:49:33 -0500

    aws push 2

--- config.py
+++ config.py
@@ -9,5 +9,4 @@ class Config(object):
   SECRET_KEY = 'Fn9kmuYCbgodkUoYgsmWKgSg5t7zjQqLyX6f2sHAey4SC2ukfo4Rjw4aJFxGQhL6'
   SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
   SQLALCHEMY_TRACK_MODIFICATIONS = False
-  RIOT_API_KEY = 'RGAPI-c8858b71-bace-4366-8bbe-eafbfc6b39e9'
                                 
