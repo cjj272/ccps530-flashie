ad7efbd99a30cfdb234e1a63e90938ccb96ae51f
Corey <38830237+cjj272@users.noreply.github.com>
2020-02-16 14:44:40 -0500

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
   DEBUG = False
   TESTING = False
   CSRF_ENABLED = True
-  SECRET_KEY = 'Fn9kmuYCbgodkUoYgsmWKgSg5t7zjQqLyX6f2sHAey4SC2ukfo4Rjw4aJFxGQhL6'
+  SECRET_KEY = 'CHANGE_THIS_TO_SOMETHING_MORE_SECURE'
   SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
   SQLALCHEMY_TRACK_MODIFICATIONS = False
                 
